ebfb8cf663bd80d7b05a28d4fa885f4e734bb48f
Carlos Cortez <ccortez@aws.pe>
2021-04-07 21:22:41 -0500

    update de Body

--- comprehend_01_nlpseries_ep_3/app.py
+++ comprehend_01_nlpseries_ep_3/app.py
@@ -12,17 +12,22 @@ def lambda_handler(event, context):
     #Accessing data
     puro = json.dumps(event) #intentando cambiar aqui sino vuelvo a la linea 13
     text = puro[0]
-    print("text Body:", text)
+    json_object = json.loads(puro)
+    print("texto a analizar: ", json_object["body"])
+    Body = json.dumps(json_object["body"])
+    print("Body:", Body)
 
     #detectamos el sentimiento 
-    sentiment = client.detect_sentiment(Text = text, LanguageCode = 'es') #llamada al API de comprehend detect_semtiment
+    print("iniciando analisis de sentimiento....")
+    sentiment = client.detect_sentiment(Text = Body, LanguageCode = 'es') #llamada al API de comprehend detect_semtiment
     sentRes = sentiment['Sentiment'] #los valores posibles son Positive, Neutral, or Negative
     sentScore = sentiment['SentimentScore'] #medicion en porcentajes de los valores conseguidos
     print(sentRes)
     print(sentScore)
 
     #Entity Extraction en Comprehend
-    entities = client.detect_entities(Text = text, LanguageCode = 'es') #llamada al API para entity extraction
+    print("iniciando extraccion de entidades....")
+    entities = client.detect_entities(Text = Body, LanguageCode = 'es') #llamada al API para entity extraction
     entities = entities['Entities'] #traer todas las entidades reconocidas
     print(entities)
     textEntities = [dict_item['Text'] for dict_item in entities] #los textos identificados como entidades
